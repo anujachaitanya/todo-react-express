26eb2d48b47aee492adef3e9ebd431f2b7c42c91
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-09-09 18:04:36 +0530

    added script to check whether dir is there

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const { app } = require('./src/app');
 const PORT = process.env.PORT || 3001;
+const fs = require('fs');
 
+const files = fs.readdirSync('./client/build');
+console.log(files);
 app.listen(PORT, () => console.log('app is listening to 3001'));
