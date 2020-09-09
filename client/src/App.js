ba2ea7ec65b275593761c78b73bf95ee916d54b3
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-09-09 18:28:55 +0530

    added shellScript to get react app

--- client/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Todo from './component/Todo';
-
-const App = function () {
-  return <Todo />;
-};
-
-export default App;
---
+++ shellScripts/getReactApp.sh
@@ -0,0 +1,7 @@
+git clone git@github.com:anujachaitanya/todo-react.git client
+cd client 
+npm install
+npm run build 
+mv ./build/ ../
+cd ..
+rm -rf client
