ef9c1b18fc24eddec18a77fc7ff777c11303a3d1
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-09-09 18:34:16 +0530

    added shellScript to get react app

--- server.js
+++ server.js
@@ -2,6 +2,4 @@ const { app } = require('./src/app');
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 
-const files = fs.readdirSync('./client');
-console.log(files);
 app.listen(PORT, () => console.log('app is listening to 3001'));
