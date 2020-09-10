55df604b78f9c744af2b303b97ebea04ec217fe5
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-09-10 11:39:57 +0530

    used REDIS_URL

--- src/redisClient.js
+++ src/redisClient.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 
 const getRedisClient = function () {
-  console.log(process.env.REDISCLOUD_URL, '>>>>>>>>>');
+  console.log(process.env.REDISCLOUD_URL, process.env.REDIS_URL, '>>>>>>>>>');
   if (process.env.REDISCLOUD_URL) {
     console.log('In redis cloud');
     return redis.createClient(process.env.REDISCLOUD_URL, {
